67feb165cf46f8699859492dbf4eb49af549e6b5
sharperprogrammer <sharperprogrammer@gmail.com>
2018-01-20 12:25:49 -0500

    Getting ready to deploy to app server.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   
+    # since there is no code in this Action, it will merely load home.html.erb
   def home
     
   end
   
+  # since there is no code in this Action, it will merely load about.html.erb
   def about
     
   end
